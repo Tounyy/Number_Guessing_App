cff41be3acab41e5ae7f5e09538fcfb09c0471cd
Tounyyyyy <tonyluu125576@gmail.com>
2023-07-30 14:32:35 +0200

    fix bug in app

--- app.py
+++ app.py
@@ -1,28 +1,39 @@
 import streamlit as st
 import random
 
-if 'state' not in st.session_state:
-    st.session_state['state'] = {'lives': 4}
+# Initialization Session State
+if 'lives' not in st.session_state:
+    st.session_state['lives'] = 3
 
+# Nadpis
 st.markdown("<h1 style='text-align: center; color: white; font-size: 35px;'> Number Guessing Game </h1>", unsafe_allow_html=True)
 
+# Počítač
 select = range(1, 11)
-computer = random.choice(select)
-player = st.number_input("Vyberte číslo:", min_value=1, max_value=max(select), step=1)
+if 'computer' not in st.session_state:
+    st.session_state['computer'] = random.choice(select)
 
-if st.button('Guess'):
-    if computer == player:
-        st.markdown("<h1 style='text-align: center; color: white; font-size: 25px;'>Vyhrali jste hru!</h1>",
-                    unsafe_allow_html=True)
+# Hráč
+player = st.number_input("Vyberte číslo:", min_value=1,max_value=max(select), step=1)
+
+# Funkce restartování hry
+def reset_game():
+    st.session_state['lives'] = 3
+    st.session_state['computer'] = st.session_state['computer'] = random.choice(select)
+
+if st.button('Click'):
+    if st.session_state['computer'] == player:
+        st.markdown("<h1 style='text-align: center; color: white; font-size: 25px;'>Vyhrali jste hru!</h1>", unsafe_allow_html=True)
         st.write("Číslo, které jste si vybral: " + str(player))
-        st.write("Číslo, které si robot vybral: " + str(computer))
-    elif computer > player or player > computer:
-        st.session_state['state']['lives'] -= 1
-        st.markdown("<h1 style='text-align: center; color: white; font-size: 25px;'>Prohráli jste hru!</h1>", unsafe_allow_html=True)
+        st.write("Číslo, které si robot vybral: " + str(st.session_state['computer']))
+        reset_game()
+    elif st.session_state['computer'] > player or player > st.session_state['computer']:
+        st.write("Zbývající životy:", st.session_state.lives)
+        st.session_state['lives'] -= 1
         st.write("Číslo, které jste si vybral: " + str(player))
-        st.write("Číslo, které si robot vybral: " + str(computer))
-        st.write("Zbývající životy:", st.session_state['state']['lives'])
-        if st.session_state['state']['lives'] == 0:
-            st.markdown("<h1 style='text-align: center; color: white; font-size: 25px;'>Vyčerpali jste všechny životy.</h1>", unsafe_allow_html=True)
-            st.button('Resetovat')
-            st.session_state['state']['lives'] = 4
+    if st.session_state['lives'] == -1:
+        st.markdown("<h1 style='text-align: center; color: white; font-size: 25px'>Prohráli jste hru!</h1>", unsafe_allow_html=True)
+        st.write("Číslo, které si robot vybral: " + str(st.session_state['computer']))
+        reset_game()
+    if st.button("Restartovat Hru"):
+        reset_game()
